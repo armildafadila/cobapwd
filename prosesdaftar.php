023574927337d4bebf85c3fbcc4faa7f5bb3e1ca
124250034-bit <124250034@student.upnyk.ac.id>
2026-05-03 00:03:59 +0700

    tes

--- prosesdaftar.php
+++ prosesdaftar.php
@@ -6,4 +6,16 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $confirm = $_POST['confirm_pw'];
 
-if ($password)
+if ($password != $confirm) {
+    echo "Konfirmasi Password Berbeda, Silahkan ulangi lagi! <a href='daftar.php></a>";
+    exit;
+}
+$query= mysqli_query($konek, "INSERT INTO users (nama, email, password) VALUES ('$nama', '$email', '$password')") or die(mysqli_error($konek));
+
+if ($query) {
+    echo "Proses pendaftaran berhasil";
+
+}else {
+    echo "Proses pendaftaran gagal!";
+}
+?>
