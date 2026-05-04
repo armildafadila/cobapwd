2c1d91512adff3123fdc13f31190a3ef7cc8efde
124250034-bit <124250034@student.upnyk.ac.id>
2026-05-04 15:35:54 +0700

    yey

--- proseslogin.php
+++ proseslogin.php
@@ -5,14 +5,14 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $data = mysqli_query($koneksi, "SELECT * from users WHERE email='$email' AND password='$password'");
-
+$result = mysqli_query($koneksi, $data);
 $cek = mysqli_num_rows($data);
 
 if($cek > 0) {
     $_SESSION['email'] = $email;
-    $_SESSION['status'] = "login";
-    header("location:kelas10.html");
+    header("location:kelas10.php");
 
 } else {
-    header("location:masuk.php");
+    header("location: masuk.php?error=1");
+    exit;
 } ?>
