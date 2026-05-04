857b14d2dab920620866c87fbc46a25cf85a6cea
Armilda <armildafadila@gmail.com>
2026-05-04 11:17:43 +0700

    hah

--- koneksi.php
+++ koneksi.php
@@ -6,7 +6,7 @@ $database = "mathpartner";
 
 $koneksi = new mysqli ($hostname, $username, $password, $database);
 if ($koneksi->connect_error) {
-    die('maaf koneksi gagal '. $koneksi->$connect_error);
+    die('maaf koneksi gagal '. $koneksi->connect_error);
 
 }
 ?>
