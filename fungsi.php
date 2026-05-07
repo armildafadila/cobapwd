f4b7d4a1796fe7c2387dabfeb1dd9919eb2b0ad0
124250034-bit <124250034@student.upnyk.ac.id>
2026-05-07 13:09:53 +0700

    p

--- fungsi.php
+++ fungsi.php
@@ -1,6 +1,40 @@
 <?php
 $materi_fungsi = [
     'soal1' => [ 
-        ''
-    ]
+        'soal' => 'Diketahui fungsi f(x)=2x+3. Nilai dari f(4) adalah',
+        'opsi' => [
+            'A' => 8 , 'B' => 10, 'C' => 11, 'D' => 13, 'E' => 15
+        ],
+        'jawaban' => 'C'
+    ],
+    'soal2' => [ 
+        'soal' => 'Diketahui fungsi f(x)=x^2−5x+6. Nilai dari f(2) adalah',
+        'opsi' => [
+            'A' => 0 , 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4
+        ],
+        'jawaban' => 'A',
+        'pembahasan' => 'Substitusikan x = 4 ke fungsi. f(4)'
+    ],
+    'soal3' => [ 
+        'soal' => 'Jika f(x)=3x−7, maka nilai f(−2) adalah',
+        'opsi' => [
+            'A' => -13, 'B' => -12, 'C' => -11, 'D' => -10, 'E' => -9
+        ],
+        'jawaban' => 'A'
+    ],
+    'soal4' => [ 
+        'soal' => 'Diketahui fungsi f(x) = x + 4 / 2. Nilai dari f(6) adalah',
+        'opsi' => [
+            'A' => 3, 'B' => 4, 'C' => 5, 'D' => 6, 'E' => 7
+        ],
+        'jawaban' => 'C'
+    ],
+    'soal5' => [ 
+        'soal' => 'Diketahui fungsi f(x) = x^2 + 2x - 8. Nilai dari f(3) adalah',
+        'opsi' => [
+            'A' => 5, 'B' => 6, 'C' => 7, 'D' => 8, 'E' => 9
+        ],
+        'jawaban' => 'C'
+    ],
+
 ]
